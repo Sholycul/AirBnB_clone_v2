da195684e776a8f821b62a5e8e55ef0c03cd01ec
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-05-04 20:30:21 -0700

    set up web server for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 #Setup for deployment
 
-# Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 # Create necessary folders
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 
 # Create fake HTML file
 echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
@@ -18,7 +23,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-sudo sed -i 's|^.*server_name.*$|&\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 
 # Restart Nginx
