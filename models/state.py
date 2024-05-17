07871870c4f7cac93ed7a1d1292f6a586a6b04ef
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-05-16 21:12:20 -0700

    Under review

--- models/state.py
+++ models/state.py
@@ -10,14 +10,20 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state',
-                          cascade='all, delete-orphan')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if storage_type != 'db':
-        @property
-        def cities(self):
-            """ Getter method for cities related to the current State """
-            from models.city import City
-
-            return [city for city in storage.all(City).values()
-                    if getattr(city, 'state_id') == self.id]
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
