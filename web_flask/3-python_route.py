89cd95bee1e89762b8e5efb340d185b7f1e6cbf8
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-05-16 20:02:46 -0700

    Displays Python with texts

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" This is our serviced homepage """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    This hello HBNB home page
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_page():
+    """
+    This page displays HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_page(text):
+    """
+    This page displays C with some provided text
+    """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_page(text):
+    """
+    This page displays Python with some provided text
+    If no text provided, it renders 'is cool'
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
